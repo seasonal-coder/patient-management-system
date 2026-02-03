9fd4045091d97fe18570c9ca16ef25ce1f0a72c4
seasonal-coder <kalaam.9k@gmail.com>
2026-02-03 11:14:00 +0530

    done custom EmailAlreadyExists exception

---
+++ patient-service/src/main/java/com/kalam/patientservice/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.kalam.patientservice.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+  public EmailAlreadyExistsException(String message) {
+    super(message);
+  }
+}
